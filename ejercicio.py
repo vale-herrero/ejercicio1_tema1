4e7e14ed0daa192dcf32c15dff1218988ef97597
vale-herrero <101349987+vale-herrero@users.noreply.github.com>
2022-09-22 20:14:57 +0200

    se completó el ejercicio 1

---
+++ ejercicio.py
@@ -0,0 +1,21 @@
+cadena= "zeréP nauJ,01"
+
+J= cadena[-4]
+u= cadena[-5]
+a= cadena[-6]
+n= cadena[-7]
+
+nombre= J+u+a+n
+
+P= cadena[4]
+e=cadena[3]
+r=cadena[2]
+e=cadena[1]
+z=cadena[0]
+
+apellido= P+e+r+e+z
+nota1=cadena[-2]
+nota2=cadena[-1]
+nota=nota1+nota2
+
+print(nombre, apellido, "ha sacado un", nota, "de nota")
